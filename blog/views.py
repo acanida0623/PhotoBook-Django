2ffab7ce35ed91bb998c4579537c0ff4a90b171a
ANDREW CANIDA <MR.ANDREW.CANIDA@GMAIL.COM>
2016-04-28 00:12:38 -0700

    first commit, ajax post and get requests are 200, need to fix url save

---
+++ blog/views.py
@@ -0,0 +1,125 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render_to_response
+from django.utils import timezone
+from .models import Post, Comment, Image
+from .forms import PostForm, CommentForm
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+
+
+def write_images():
+    with open('urls.txt', 'w') as outfile:
+        json.dump(images, outfile)
+
+def read_images():
+    with open('urls.txt', 'r') as infile:
+        images = json.load(infile)
+        print("hello")
+        print(images['images'])
+
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {
+        'posts': Post.objects.all()
+    })
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+@login_required
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+@login_required
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+@login_required
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+@login_required
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('blog.views.post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('blog.views.post_detail', pk=post_pk)
+
+@csrf_exempt
+def save_urls(request):
+    if request.is_ajax():
+        print('1')
+        print(request)
+        if request.method == 'POST':
+           url_list = Image(url='google.com', row=2)
+           url_list.save()
+           return HttpResponse( request )
+
+@csrf_exempt
+def get_urls(request):
+    if request.is_ajax():
+        if request.method == 'GET':
+            url_list = []
+            for name in Image.objects.all():
+                url_list.append(name.url)
+            return HttpResponse(
+            json.dumps(url_list)
+            )
